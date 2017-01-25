d433661b571e2fa2cd508241f0b8afbc3d2410c9
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-01-25 00:03:04 -0600

    First functioning user-signup form

--- main.py
+++ main.py
@@ -1,25 +1,111 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import cgi
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign UP</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <a href="/">User Signup</a>
+    </h1>
+"""
+page_footer = """
+</body>
+</html>
+"""
+empty_space = " "
+class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.flicklist.com/
+    """
     def get(self):
-        self.response.write('Hello world!')
+
+        user_signup_form = """
+        <form action="/userverify" method="post">
+            <label>
+            Username
+                <input type="text" name="username"/><br>
+            </label><br>
+            <label>
+            Password
+                <input type="text" name="password"/><br>
+            </label><br>
+            <label>
+            Confirm Pasword
+                <input type="text" name="confirm"/><br>
+            </label><br>
+            <label>
+            Email(Optional)
+                <input type="text" name="email"/><br>
+            </label>
+            <buton><input type="submit" value="Submit"/></button>
+        </form>
+        """
+        error = self.request.get("error")
+        error_element = "<p class='error'>" + error + "</p>" if error else ""
+        main_content = page_header + user_signup_form + error_element
+        content = main_content + page_footer
+        self.response.write(content)
+class UserVerify(webapp2.RequestHandler):
+    """ Handles requests coming in to '/add'
+        e.g. www.flicklist.com/add
+    """
+    def post(self):
+        user_name = self.request.get("username")
+        password = self.request.get("password")
+        confirm_password = self.request.get("confirm")
+        email =self.request.get("email")
+        if (empty_space in user_name):
+            error = "'{0}' Please provide valid username".format(user_name)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+
+        elif (len( user_name)<1):
+            error = "'{0}' Please provide valid username".format(user_name)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+        main_content = "<strong>"+ "Well Come, " + user_name.capitalize() +"</strong>"
+        content =  main_content + page_footer
+        self.response.write(content)
+        if (empty_space in password):
+            error = "'{0}' Please provide valid password".format(password)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+
+        elif (len( password)<1):
+            error = "'{0}' Please provide valid password".format(password)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+
+        content =   ''
+        self.response.write(content)
+        if (confirm_password == password)==False:
+            error = "'{0}' Password Verification failed".format(confirm_password)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+        content =   ''
+        self.response.write(content)
+        if (empty_space in email):
+            error = "'{0}' Please provide valid email".format(email)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+
+        elif (len( email)<1):
+            error = "'{0}' Please provide valid username".format(email)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+        content =   ''
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/userverify', UserVerify)
 ], debug=True)
