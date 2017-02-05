3dc5d316c30224b8012eca7f14e6992470d8ef4a
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-02-04 18:37:23 -0600

    further updates to the user signup

--- main.py
+++ main.py
@@ -1,105 +1,66 @@
-
+import os
 import webapp2
 import cgi
-page_header = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Sign UP</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <h1>
-        <a href="/">User Signup</a>
-    </h1>
-"""
-page_footer = """
-</body>
-</html>
-"""
-empty_space = " "
-class Index(webapp2.RequestHandler):
-    def get(self):
+import re
+import jinja2
 
-        user_signup_form = """
-        <div><form action="/userverify" method="post">
-            <label>
-            <span style="display:inline-block; padding-right:48px;">Username</span>
-                <input type="text" name="username"/><br>
-            </label><br>
-            <label>
-            <span style="display:inline-block; padding-right:50px;">Password</span>
-                <input type="text" name="password"/><br>
-            </label><br>
-            <label>
-            Confirm Pasword
-                <input type="text" name="confirm"/><br>
-            </label><br>
-            <label>
-            <span style="display:inline-block; padding-right:5px;">Email(Optional)</span>
-                <input type="text" name="email"/><br>
-            </label>
-            <buton><input type="submit" value="Submit"/></button>
-        </form></div>
-        """
-        error = self.request.get("error")
-        error_element = "<p class='error'>" + error + "</p>" if error else ""
-        main_content = page_header + user_signup_form + error_element
-        content = main_content + page_footer
-        self.response.write(content)
-class UserVerify(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+class Handler(webapp2.RequestHandler):
+    def renderError(self, error):
+        self.response.write(error)
+
+class UserVerify(Handler):
+    def get(self):
+        t = jinja_env.get_template("signup-form.html")
+        content = t.render(error_escaped=self.request.get("error"))
+        self.response.write (content)
     def post(self):
         user_name = self.request.get("username")
         password = self.request.get("password")
         confirm_password = self.request.get("confirm")
         email =self.request.get("email")
-        if (empty_space in user_name):
-            error = "'{0}' Please provide valid username".format(user_name)
+        if not valid_username(user_name):
+            error = " Please provide valid username"
             error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-
-        elif (len( user_name)<1):
-            error = "'{0}' Please provide valid username".format(user_name)
+            t = jinja_env.get_template("signup-form.html")
+            content = t.render( error_escaped = error_escaped)
+            self.response.write (content)
+        if not valid_password(password):
+            error = " Please provide valid password"
             error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-        main_content = "<strong>"+ "Well Come, " + user_name.capitalize() +"</strong>"
-        content =  main_content + page_footer
-        self.response.write(content)
-        if (empty_space in password):
-            error = "'{0}' Please provide valid password".format(password)
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-
-        elif (len( password)<1):
-            error = "'{0}' Please provide valid password".format(password)
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-
-        content =   ''
-        self.response.write(content)
+            t = jinja_env.get_template("signup-form.html")
+            content = t.render( error_escaped = error_escaped)
+            self.response.write (content)
         if (confirm_password == password)==False:
-            error = "'{0}' Password Verification failed".format(confirm_password)
+            error = "Password Verification failed"
             error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-        content =   ''
-        self.response.write(content)
-        if (empty_space in email):
-            error = "'{0}' Please provide valid email".format(email)
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
+            t = jinja_env.get_template("signup-form.html")
+            content = t.render( error_escaped = error_escaped)
+            self.response.write (content)
 
-        elif (len( email)<1):
-            error = "'{0}' Please provide valid username".format(email)
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-        content =   ''
-        self.response.write(content)
+        self.redirect('/welcome?username=' + user_name)
+class Welcome(Handler):
+    def get(self):
+        username = self.request.get("username")
+        if valid_username(username):
+            self.response.write("Welcome, " + username)
+        else:
+            self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/', Index),
-    ('/userverify', UserVerify)
+    ('/', UserVerify),
+    ('/welcome', Welcome),
 ], debug=True)
