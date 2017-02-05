11c8c8b40d6631d863cf83124e41d843d374f5b4
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-02-04 19:15:59 -0600

    Tentative Usersignup

--- main.py
+++ main.py
@@ -19,48 +19,51 @@ EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 class Handler(webapp2.RequestHandler):
-    def renderError(self, error):
-        self.response.write(error)
-
-class UserVerify(Handler):
+    def renderError(self, error_code):
+        self.error(error_code)
+        self.response.write("Oops! Something went wrong.")
+class Index(Handler):
     def get(self):
         t = jinja_env.get_template("signup-form.html")
         content = t.render(error_escaped=self.request.get("error"))
         self.response.write (content)
+class UserVerify(Handler):
     def post(self):
+
         user_name = self.request.get("username")
         password = self.request.get("password")
         confirm_password = self.request.get("confirm")
         email =self.request.get("email")
+        error = False
         if not valid_username(user_name):
             error = " Please provide valid username"
             error_escaped = cgi.escape(error, quote=True)
-            t = jinja_env.get_template("signup-form.html")
-            content = t.render( error_escaped = error_escaped)
-            self.response.write (content)
+            error = True
         if not valid_password(password):
             error = " Please provide valid password"
             error_escaped = cgi.escape(error, quote=True)
-            t = jinja_env.get_template("signup-form.html")
-            content = t.render( error_escaped = error_escaped)
-            self.response.write (content)
+            error = True
         if (confirm_password == password)==False:
             error = "Password Verification failed"
             error_escaped = cgi.escape(error, quote=True)
+        if error:
+
             t = jinja_env.get_template("signup-form.html")
             content = t.render( error_escaped = error_escaped)
             self.response.write (content)
-
-        self.redirect('/welcome?username=' + user_name)
-class Welcome(Handler):
-    def get(self):
-        username = self.request.get("username")
-        if valid_username(username):
-            self.response.write("Welcome, " + username)
         else:
-            self.redirect('/')
+
+
+            self.response.write("Welcome, " + user_name)
+#class Welcome(Handler):
+    #def get(self):
+    #    username = self.request.get("username")
+    #    if valid_username(username):
+    #        self.response.write("Welcome, " + username)
+    #    else:
+    #        self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/', UserVerify),
-    ('/welcome', Welcome),
+    ('/', Index),
+    ('/userverify', UserVerify),
 ], debug=True)
